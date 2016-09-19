a812d534984b6086a0b255c25c5ee5b551d19a75
blue0knight <oni_rivera98@yahoo.com>
2016-09-19 19:24:54 -0400

    optimized and refactored

--- js/script.js
+++ js/script.js
@@ -11,19 +11,30 @@ $( document ).ready(function() {
   //show first slide
   $('.active').show();
 
-  $('#next').on('click', function(){
+  //next handler
+  $('#next').on('click',nextSlide);
+  //prev handler
+  $('#prev').on('click',prevSlide);
+  //auto slider handler
+  if(autoswitch == true){
+    setInterval(nextSlide, autoswitch_speed);
+  }
+
+  //switch to next slide
+  function nextSlide(){
     $('.active').removeClass('active').addClass('oldActive');
     if($('.oldActive').is(':last-child')){
       $('.slide').first().addClass('active');
     } else {
       $('.oldActive').next().addClass('active');
     }
-    $('oldActive').removeClass('oldActive');
+    $('.oldActive').removeClass('oldActive');
     $('.slide').fadeOut(speed);
     $('.active').fadeIn(speed);
-  })
+  }
 
-  $('#prev').on('click', function(){
+  //switch to prev slide
+  function prevSlide(){
     $('.active').removeClass('active').addClass('oldActive');
     if($('.oldActive').is(':first-child')){
       $('.slide').last().addClass('active');
@@ -33,19 +44,5 @@ $( document ).ready(function() {
     $('.oldActive').removeClass('oldActive');
     $('.slide').fadeOut(speed);
     $('.active').fadeIn(speed);
-  })
-
-  if(autoswitch == true){
-    setInterval(function(){
-      $('.active').removeClass('active').addClass('oldActive');
-      if($('.oldActive').is(':last-child')){
-        $('.slide').first().addClass('active');
-      } else {
-        $('.oldActive').next().addClass('active');
-      }
-      $('oldActive').removeClass('oldActive');
-      $('.slide').fadeOut(speed);
-      $('.active').fadeIn(speed);  
-    }, autoswitch_speed);
   }
 });
